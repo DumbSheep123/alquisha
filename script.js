1594c2a880dc924bc556c727e41e402a3d6a3f89
Andrew Frykman <andrewfrykman123@icloud.com>
2023-05-05 14:16:38 -0400

    Coming Soon

--- script.js
+++
@@ -1,6 +0,0 @@
-const button = document.getElementById("button");
-const message = document.getElementById("message");
-
-button.addEventListener("click", () => {
-	message.innerText = "You clicked the button! Congrats!";
-});
